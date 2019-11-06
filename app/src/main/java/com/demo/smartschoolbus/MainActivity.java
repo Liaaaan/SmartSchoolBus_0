1c466265e6775d4baf8fc2f0d6df807f2740b0f6
Liaaaan <52323815+Liaaaan@users.noreply.github.com>
2019-11-06 19:54:46 +0800

    显示地图

---
+++ app/src/main/java/com/demo/smartschoolbus/MainActivity.java
@@ -0,0 +1,35 @@
+package com.demo.smartschoolbus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends Activity {
+    private MapView mMapView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+}
